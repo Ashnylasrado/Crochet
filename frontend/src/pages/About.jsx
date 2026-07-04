3e8fbd994696623ccb76d50e8d1bee8dcc736bfd
Ashny Lasrado <ashnylasrado1998@gmail.com>
2026-07-04 15:09:04 -0400

    about changes

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -9,9 +9,12 @@ export default function About() {
         hobby has grown into a little shop full of cozy hats, huggable amigurumi,
         cheerful home decor, and everyday bags — all made with care.
       </p>
+      <h2>Meet the Founder</h2>
+      <img src="/product-images/photo.PNG"/>
+      <p>Ashny Lasrado</p>
+      <p>Toronto, Canada M5A 0B9</p>
       <p>
-        This project is a learning build: a React frontend talking to a Node.js/Express
-        backend, deployed on a live server. Thanks for stopping by!
+         Thanks for stopping by!
       </p>
     </div>
   );
