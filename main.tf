cff1bccf537308d7a82473d90c3d418f562632fb
Ashnylasrado <47290415+Ashnylasrado@users.noreply.github.com>
2026-07-20 13:09:03 -0400

    Change instance type from t2.micro to t3.micro

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "demo" {
   ami           = "ami-0c02fb55956c7d316"  # example Amazon Linux AMI, verify it's valid for your region
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "github-actions-demo"
